73ba2644a7b4ed894a6c95b638d7b269316ea8e6
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-04-27 23:00:40 +0100

    MAP - standalone component - interractive (hover)

---
+++ src/app/components/tn-map/tn-map.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tn-map',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tn-map.component.html',
+  styleUrls: ['./tn-map.component.scss']
+})
+export class TnMapComponent {
+
+}
