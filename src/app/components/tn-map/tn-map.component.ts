9907630973351c868363a1eeef6d510637ccad61
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-04-28 14:16:33 +0100

    Setting state links dynamically

--- src/app/components/tn-map/tn-map.component.ts
+++ src/app/components/tn-map/tn-map.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TN_STATE_INFO } from 'src/app/utils/mapdata';
 
 @Component({
   selector: 'app-tn-map',
@@ -8,6 +9,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './tn-map.component.html',
   styleUrls: ['./tn-map.component.scss']
 })
-export class TnMapComponent {
+export class TnMapComponent implements AfterViewInit {
 
+  @ViewChild('tunisianMap') svg!: ElementRef<SVGElement>;
+  STATES_IN_MAP: any[] = [];
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.STATES_IN_MAP = Array.from(this.svg.nativeElement.childNodes);
+    this.setMapConfiguration();
+  }
+
+
+  setMapConfiguration() {
+
+    for (let index = 0; index < (this.STATES_IN_MAP.length - 3); index++) {
+      const svgAnchorElement = this.STATES_IN_MAP[index]; // outer anchor tag that holds a path
+      const svgPathElement = svgAnchorElement['firstChild']; // actual path element
+      const svgPathID = svgAnchorElement['firstChild'].id; // ID assogned to each path, used to reference state properties in imported object
+      const STATE = TN_STATE_INFO[svgPathID];
+
+      // Setting dynamic links for each Anchor element
+      this.renderer.setAttribute(svgAnchorElement, 'href', `https://www.google.com/search?q=${STATE?.name}`);
+    }
+  }
 }
