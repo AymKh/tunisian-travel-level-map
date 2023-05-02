924eaea584ca557ad729effb2aea646b46445ea8
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-05-02 21:24:04 +0100

    injecting custom directive to all anchor tags

--- src/app/components/tn-map/tn-map.component.ts
+++ src/app/components/tn-map/tn-map.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TN_STATE_INFO } from 'src/app/utils/mapdata';
+import { DirectivesModule } from 'src/app/directives/directives.module';
 
 @Component({
   selector: 'app-tn-map',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DirectivesModule],
   templateUrl: './tn-map.component.html',
   styleUrls: ['./tn-map.component.scss']
 })
@@ -27,10 +28,15 @@ export class TnMapComponent implements AfterViewInit {
     for (let index = 0; index < (this.STATES_IN_MAP.length - 3); index++) {
       const svgAnchorElement = this.STATES_IN_MAP[index]; // outer anchor tag that holds a path
       const svgPathID = svgAnchorElement['firstChild'].id; // ID assogned to each path, used to reference state properties in imported object
+      const svgPathElement = svgAnchorElement['firstChild'];
       const STATE = TN_STATE_INFO[svgPathID]; // state properties imported from the global object
 
       // Setting dynamic links for each Anchor element
       this.renderer.setAttribute(svgAnchorElement, 'href', `https://www.google.com/search?q=${STATE?.name}`);
+
+      // Setting custoim hover directive for each  Anchor element
+      this.renderer.setAttribute(svgAnchorElement, 'ShowStateNameOnHover', '');
+      this.renderer.setAttribute(svgAnchorElement, 'tooltipBody', 'state loading...');
     }
   }
 }
